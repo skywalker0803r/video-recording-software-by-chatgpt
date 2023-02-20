d99123467225838eb2c1799def84e31b035a95ad
skywalker0803r <41248543+skywalker0803r@users.noreply.github.com>
2023-02-20 17:10:39 +0800

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,52 @@
+from PIL import ImageGrab
+import numpy as np
+import cv2
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+# 設定錄音參數
+fs = 44100  # 取樣頻率
+seconds = 10  # 持續錄音的秒數
+
+# 創建 OpenCV 視窗
+cv2.namedWindow('screen', cv2.WINDOW_NORMAL)
+
+# 獲取螢幕大小
+image = ImageGrab.grab()
+width, height = image.size
+
+# 設定影片編碼器
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+# 創建影片寫入器
+video = cv2.VideoWriter('test.avi', fourcc, 25, (width, height))
+
+# 開始錄音
+print("Start recording...")
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+
+while True:
+    # 獲取螢幕畫面
+    img_rgb = ImageGrab.grab()
+    img_bgr = cv2.cvtColor(np.array(img_rgb), cv2.COLOR_RGB2BGR)
+    
+    # 寫入畫面
+    video.write(img_bgr)
+    
+    # 顯示畫面
+    cv2.imshow('screen', img_bgr)
+
+    # 如果按下 q 鍵，則結束迴圈
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# 結束錄音
+sd.wait()
+write('output.wav', fs, myrecording)
+
+# 釋放影片寫入器，關閉 OpenCV 視窗
+video.release()
+cv2.destroyAllWindows()
+
+# 完成
+print("Done!")
